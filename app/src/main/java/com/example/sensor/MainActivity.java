ec19c3568822cda9e4bad6f06aa7afd2fedf4b69
Abdelfattah Mohamed <abdelfata7mohammed@gmail.com>
2020-07-24 22:25:53 +0200

    FILE STREAM, APPEND & INTERNAL STORAGE

--- app/src/main/java/com/example/sensor/MainActivity.java
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -1,21 +1,53 @@
 package com.example.sensor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     // there was a mistake in name of gryscope -> Gyroscope
     //  variables
     private Accelerometer accelerometer;
     private Gryscope gryscope;
+    private static final String FILE_NAME = "Results.txt";
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    // TXT GUI
+    TextView translation_x;
+    TextView translation_y;
+    TextView translation_z;
+    // TXT GUI
+    TextView rotation_x;
+    TextView rotation_y;
+    TextView rotation_z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // GUI
+        translation_x = (TextView) findViewById(R.id.translation_x);
+        translation_y = (TextView) findViewById(R.id.translation_y);
+        translation_z = (TextView) findViewById(R.id.translation_z);
+        // GUI
+        rotation_x = (TextView) findViewById(R.id.rotation_x);
+        rotation_y = (TextView) findViewById(R.id.rotation_y);
+        rotation_z = (TextView) findViewById(R.id.rotation_z);
+
 
         // Here Take Objects
         accelerometer = new Accelerometer(this);
@@ -24,20 +56,19 @@ public class MainActivity extends AppCompatActivity {
         // Listener for accelerometer
         accelerometer.setListener(new Accelerometer.Listener() {
             @Override
-            public void onTranslation(float last_x, float last_y, float last_z) {
-                // Here Just Test Method , then will remove it and get values and save it in file
+            public void onTranslation(float last_x, float last_y, float last_z) throws IOException {
                 /*
-                if(last_x > 1.0f){
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
-                }
-                else if(last_x < -1.0f){
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                }
+                FILE STREAM ( GET DATA & PASS IT IN FILE )
                  */
-                //
-                //Toast.makeText(MainActivity.this, last_x + "X-axis", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, last_y + "Y-axis", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, last_z + "Z-axis", Toast.LENGTH_SHORT).show();
+                // Variables
+                translation_x.setText(last_x + "");
+                translation_y.setText(last_x + "");
+                translation_z.setText(last_x + "");
+                // COMMA SEPARATED DATA FILES
+                String text = last_x + "," + last_y + "," + last_z + ",";
+                byte[] text_byte = text.getBytes();
+                // WRITE HERE
+                writeToFile(text_byte);
             }
         });
 
@@ -46,24 +77,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onRotation(float rotate_x, float rotate_y, float rotate_z) {
                 /*
-                if(rotate_z > 1.0f){
-                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
-                }
-                else if(rotate_z < -1.0f){
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-
-                }
+                FILE STREAM ( GET DATA & PASS IT IN FILE )
                  */
-                Toast.makeText(MainActivity.this, rotate_x + "RX-axis", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, rotate_y + "RY-axis", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, rotate_z+ "RZ-axis", Toast.LENGTH_SHORT).show();
+                // Variables
+                rotation_x.setText(rotate_x + "");
+                rotation_y.setText(rotate_y + "");
+                rotation_z.setText(rotate_z + "");
+                // Array of objects
+                String text = rotate_x + "," + rotate_y + "," + rotate_z + "\n";
+                byte[] text_byte = text.getBytes();
+                // Write DATA
+                writeToFile(text_byte);
+
             }
         });
     }
 
-    // Methods
+    // Methods start , capture and end
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
 
         accelerometer.register();
@@ -71,10 +103,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
 
         accelerometer.unregister();
         gryscope.unregister();
     }
+
+    /*
+    FILE STREAM APPEND DATA
+     */
+    public void writeToFile(byte[] text){
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("result.txt", MODE_APPEND);
+            fileOutputStream.write(text);
+            fileOutputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+    END
+     */
+
 }
+
